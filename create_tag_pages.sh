7fcd6bac1b4c8d5f721b9d3d70c87c1c1faa5ff0
Marie Katrine Ekeberg <spacemarky@gmail.com>
2020-08-07 22:08:47 +0200

    Created a very simple script that will be used with github actions to create new tag websites when needed.

---
+++ create_tag_pages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+TAGS=$(cat _posts/*.org | grep tags | sed -E 's/tags: (.)/\1/' | tr ',' '\n' | sort -u)
+
+for tag in $TAGS
+do
+	if [ ! -f $tag.html ]
+	then
+		echo -e "---\ntitle: Tag: $tag\nlayout: tag\ntag: $tag\n---" > tags/$tag.html
+		git add tags/$tag.html
+	fi
+done
