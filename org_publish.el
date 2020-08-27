6d9ad674f0260ecd7846566070a1461707769fd3
Marie Katrine Ekeberg <mke@themkat.net>
2020-08-27 20:17:55 +0200

    Added some basic Emacs package setup in hopes of getting pipeline to work with source code blocks.

--- org_publish.el
+++ org_publish.el
@@ -1,3 +1,13 @@
+;; basic setup to get source code exports to work
+(require 'package)
+(setq package-archives '(("gnu"   . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")
+                         ("org"   . "https://orgmode.org/elpa/")))
+(package-refresh-contents)
+(package-install 'org)
+(package-install 'htmlize)
+
+
 (setq org-publish-project-alist
       '(("main-site"
          :base-directory "org"
