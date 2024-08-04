703bbe5dc3192e540fa9388d31c88e9fa4a000e4
Marie Katrine Ekeberg <mke@themkat.net>
2024-08-04 10:06:20 +0200

    Rewrite pipeline to not push to git, just deploy site directly
    Generate everything by need hopefully

--- create_tag_pages.sh
+++ create_tag_pages.sh
@@ -2,11 +2,12 @@
 
 TAGS=$(cat _posts/*.html | grep 'tags:' | sed -E 's/tags: (.)/\1/' | tr ' ' '\n' | sort -u)
 
+mkdir -p tags
+
 for tag in $TAGS
 do
 	if [ ! -f tags/$tag.html ]
 	then
 		echo -e "---\ntitle: \"Tag: $tag\"\nlayout: tag\ntag: $tag\n---" > tags/$tag.html
-		git add tags/$tag.html
 	fi
 done
