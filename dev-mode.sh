8fe6f73c3e900716b10084b3074d55296782fd7a
Marie Katrine Ekeberg <mke@themkat.net>
2024-08-12 16:59:04 +0200

    [SKIP] Minor cleanup

--- dev-mode.sh
+++ dev-mode.sh
@@ -8,7 +8,6 @@
 # Monitoring for org-mode changes
 # TODO: deleted? Removed did not work on OS X...
 # TODO: publish each file as it is
-# regular piping exits too early for some reasons. Over-engineering with xargs
 fswatch --event Created --event Updated -l 3 org | xargs -n 1 sh -c 'echo "Converting org mode files to html..." && ./emacs_headless_publish.sh && ./create_tag_pages.sh' &
 
 # base setup and main jekyll process
