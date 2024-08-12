68b2db69d246e4aaf893720539d3fe48cf41d02a
Marie Katrine Ekeberg <mke@themkat.net>
2024-08-12 17:01:05 +0200

    Move script to sub-directory to make root cleaner

--- scripts/emacs_headless_publish.sh
+++ scripts/emacs_headless_publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-emacs -Q --script org_publish.el --eval "(progn (require 'org)(org-publish-project \"themkat\"))"
+emacs -Q --script org_publish.el --eval "(progn (require 'org)(org-publish-project \"themkat\" t))"
 
 # For now the emacs command fails because of invalid regex. Still publishes, so just saying f**k it :P 
 echo "Did stuff"
